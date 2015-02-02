1de13725531e8d3cd7093dd4df41f6cff5a0b073
Garima Singh <igarimasingh@gmail.com>
2015-02-02 20:40:41 +0800

    moving spec in correct folder

---
+++ SignalRClientObjCTests/Transports/SRAutoTransportSpec.m
@@ -0,0 +1,9 @@
+//
+//  SRAutoTransportSpec.m
+//  SignalRClientObjC
+//
+//  Created by Garima Singh on 27/1/15.
+//  Copyright (c) 2015 SignalR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
